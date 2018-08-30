20b27607dc3f63f9b1ae500c40f88a9b4e65af90
4010g15 <4010g15@limon.ece.uprm.edu>
2018-08-29 20:45:45 -0400

    implemented seniority level ranking

--- src/Employee.java
+++ src/Employee.java
@@ -7,7 +7,7 @@ public class Employee {
 	public int yearService; //Years working 
 	public boolean workStatus; //Permanent [True] Temporary [False]
 	public int warnings; //Number of warnings given
-	
+
 	public Employee(String fullName, double hSalary, int hWeekly, int years, boolean status, int warnings) {
 		eFullName = fullName;
 		eHSalary = hSalary; 
@@ -16,60 +16,60 @@ public class Employee {
 		workStatus = status;
 		this.warnings = warnings;
 	}
-	
+
 	public String getFullName() {
 		return eFullName;
 	}
-	
+
 	public double getHourlySalary() {
 		return eHSalary;
 	}
-	
+
 	public int getHoursPerWeek() {
 		return hourWeekly;
 	}
-	
+
 	public int getYearsOfService() {
 		return yearService;
 	}
-	
+
 	public boolean getStatus() {
 		return workStatus;
 	}
-	
+
 	public int getWarnings() {
 		return warnings;
 	}
-	
+
 	public double getYearlySalary() {
 		return eHSalary*hourWeekly*4.5*12;
 	}
-	
+
 	public void setFullName(String fullName) {
 		eFullName = fullName;
 	}
-	
+
 	public void setHourlySalary(double hSalary) {
 		eHSalary = hSalary;
 	}
-	
+
 	public void setHoursPerWeek(int hours) {
 		hourWeekly = hours;
 	}
-	
+
 	public void setYearsOfService(int years) {
 		yearService = years;
 	}
-	
+
 	public void setStatus(boolean status) {
 		workStatus = status;
 	}
-	
+
 	public void setWarnings(int warnings) {
 		this.warnings = warnings;
 		//Comment
 	}
-	
+
 	/**
 	 * A method that returns the level of seniority of an employee. (Levels 0 to 3)
 	 * 1. The level of seniority is determined by years of service:
@@ -80,37 +80,22 @@ public class Employee {
 	 * @return 
 	 */
 	public int seniorityLevel() {
-		
+
 		//Add Code Here [You have to use If/Else to get graded]
-		
-		return 0;// Temporal Return
-		
+
+		if (yearService >= 10)
+
+			return 3;
+
+		else if (yearService >= 5)
+
+			return 2; 
+
+		else if (yearService >= 1)
+
+			return 1;
+		else return 0;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
 }

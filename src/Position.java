637ba3e9e850a205eb9fab31177758ae358c579f
harry.hernandez <harry.hernandez@jobo.ece.uprm.edu>
2018-08-29 21:15:56 -0400

    Implemented yearly salary method.

--- src/Position.java
+++ src/Position.java
@@ -69,7 +69,11 @@ public class Position {
 		
 		//Add Code Here [You have to use If/Else to get graded]
 		
-		return 0; //Temporal Return
+		if(jobType){
+			return hSalary*40*4.5*12;
+		} else{
+			return hSalary*20*4.5*12;
+		}
 		
 	}
 }
